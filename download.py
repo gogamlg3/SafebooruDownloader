9ebdd3786b9d56bfe43ee5c37a2f95858792da49
gogamlg3 <sergeydubstep1611@gmail.com>
2024-04-17 00:45:17 +0500

    fix file extensions

--- download.py
+++ download.py
@@ -14,20 +14,26 @@ args = parser.parse_args()
 
 os.makedirs(args.directory, exist_ok=True) 
 
-def get_thumbs(pid):
-    if args.url[-5:] == 'pid=0':
+def get_thumbs(pid):  # получение tuple состоящий из массива id картинок и домена сайта
+    if args.url[-5:] == 'pid=0':  # проверка на наличии pid в ссылке и отсечение этого запроса
         url = f'{args.url[0:-6]}&pid={pid}'
     else:
         url = f'{args.url}&pid={pid}'
+    
+    site = ''
+    for char in args.url[8:]:
+        if char != '/':
+            site += char
+        else:
+            break
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-    images = soup.findAll('span', class_='thumb')
+    images = []
+    while len(images) == 0: # проверка на пустую страницу в следствие перегрузки поиска
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "html.parser")
+        images = soup.findAll('span', class_='thumb')
 
-    if images == 0:
-        get_thumbs(pid)
-    else:
-        return images
+    return (images, site)
 
 def download(original_href, output_path, img_id, file_extension):
     global images_count
@@ -40,19 +46,22 @@ last_pid = args.last_page * 40 - 40
 images_count = 0
 
 while pid <= last_pid:
-    images = get_thumbs(pid)
+    images, site = get_thumbs(pid)
 
     for image in images:
         img_id = image.get('id')[1:]
-        url = f'https://safebooru.org/index.php?page=post&s=view&id={img_id}'
+        url = f'https://{site}/index.php?page=post&s=view&id={img_id}'
         page = requests.get(url)
         soup = BeautifulSoup(page.text, "html.parser")
         original_href = soup.findAll('li', string='Original image')[0].select('a[href]')[0].get('href')
-
-        if original_href[-3:] == 'peg':
-            file_extension = original_href[-4:]
-        else:
-            file_extension = original_href[-3:]
+        
+        file_extension = ''
+        for i in range(len(original_href)-1, 0, -1):
+            if original_href[i] != '.':
+                file_extension += original_href[i]
+            else:
+                file_extension = file_extension[::-1]
+                break
 
         output_path = f'{args.directory}/{img_id}.{file_extension}'
 
