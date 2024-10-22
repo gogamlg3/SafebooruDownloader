6da6a10c3cb9fc19856f3591c497343f1dad13bd
gogamlg3 <sergeydubstep1611@gmail.com>
2024-10-22 18:51:07 +0500

    add timer

--- download.py
+++ download.py
@@ -6,7 +6,7 @@ import sys
 import os
 import re
 import threading
-from time import sleep
+from time import sleep, time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url', type=str, help='url from safebooru')
@@ -21,7 +21,28 @@ pid = args.first_page * 42 - 42
 last_pid = args.last_page * 42 - 42
 images_count = 0
 
-def get_thumbs(pid):  # получение tuple состоящий из массива id картинок и домена сайта
+def timer(time_format='s', out_string="Время выполнения:"): # декоратор для замерки времени работы функций
+    def decorator(func):
+        def output_func(*args, **kwargs):
+            start_time = time()
+            result = func(*args, **kwargs)
+            end_time = time()
+            
+            func_time = end_time - start_time
+            
+            if time_format == 'ms':
+                func_time *= 1000
+                print(f"{out_string} {func_time:.2f} ms")
+
+            elif time_format == 's':
+                print(f"{out_string} {func_time:.2f} s")
+            
+            return result
+        return output_func
+    return decorator
+
+@timer('ms', "Время парсинга страницы:")
+def get_thumbs(pid):  # получение tuple состоящий из массива id картинок
     if args.url[-5:] == 'pid=0':  # проверка на наличии pid в ссылке и отсечение этого запроса
         url = f'{args.url[0:-6]}&pid={pid}'
     else:
